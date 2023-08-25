8ba488e0227708b5cb181fff919b480b200cc091
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 17:22:09 +0530

    add: grid initialised in trait

--- app/traits/page.tsx
+++ app/traits/page.tsx
@@ -30,7 +30,24 @@ export default function Home() {
           </div>
         ) : (
           <div>
-            User not initialized
+            <div className="grid grid-cols-4 gap-4" style={{ height: availableHeight }}>
+              <div>01</div>
+              <div>02</div>
+              <div>03</div>
+              <div>04</div>
+              <div>05</div>
+              <div>06</div>
+              <div>07</div>
+              <div>08</div>
+              <div>09</div>
+              <div>10</div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
+              <div>16</div>
+            </div>
           </div>
         )}
 
