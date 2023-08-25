09f16724d78a823bdb810a295c682f1a38fdd171
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 15:37:54 +0530

    add: Trait updated (1/2)

--- app/abstract/page.tsx
+++ app/abstract/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
+    
       <div>
         <Navbar/>
         hello abstract
--- app/traits/page.tsx
+++ app/traits/page.tsx
@@ -1,10 +1,27 @@
-import Navbar from '../components/navbar'
+import React from 'react';
+import Navbar from '../components/navbar';
 
 export default function Home() {
   return (
+    <div>
+      <Navbar />
+      <div className='flex'>
+
+      <input
+        type="text"
+        style={{
+          width: '70%',
+          padding: '12px 20px',
+          margin: '8px 0',
+          boxSizing: 'border-box',
+        }}
+        />
       <div>
-        <Navbar/>
-        hello
+        <button className="bg-green-500 hover:bg-green-600 text-white py-4 px-8 text-center text-lg font-semibold rounded ">
+          Click me
+        </button>
+      </div>
+      </div> 
     </div>
-  )
+  );
 }
