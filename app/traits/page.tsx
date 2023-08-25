1c2c43190d85eb20b56e53f0543ca7fe00dba3b7
Aarat Swaroop <aarat2018@gmail.com>
2023-08-25 16:01:33 +0530

    Fix:trait updated

--- app/traits/page.tsx
+++ app/traits/page.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import Navbar from '../components/navbar';
 
 export default function Home() {
+  const navbarHeight = 4;
+  const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   return (
-    <div>
+    <div style={{ height: availableHeight }}>
       <Navbar />
-      <div className='flex'>
-
-      <input
-        type="text"
-        style={{
-          width: '70%',
-          padding: '12px 20px',
-          margin: '8px 0',
-          boxSizing: 'border-box',
-        }}
+      <div className='flex -space-x-2 inset-x-0 bottom-0'>
+        <input
+          type="text"
+          style={{
+            width: '85%',
+            padding: '12px 20px',
+            margin: '0',
+            boxSizing: 'border-box',
+          }}
+          className="rounded-l-lg"
         />
-      <div>
-        <button className="bg-green-500 hover:bg-green-600 text-white py-4 px-8 text-center text-lg font-semibold rounded ">
+        <button className="bg-green-500 hover:bg-green-600 text-white py-4 px-8 text-center text-lg font-semibold rounded-r-lg w-[15%]">
           Click me
         </button>
       </div>
-      </div> 
     </div>
   );
 }
