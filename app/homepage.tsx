2b4302ea0dd42d6d3b18ee6ca857979074c3b9b0
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 14:12:02 +0530

    fix: homepage text reponsive

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -7,10 +7,10 @@ export default function Homepage() {
       <div className='md:bg-gradient-to-r from-cyan-500 to-blue-500 absolute left-0 md:h-full md:w-[50%] items-center flex justify-center h-[50%] w-full bg-gradient-to-b from-cyan-500 to-blue-500'>
         <div className="flex items-center justify-center flex-col">
         <Image src="/logo.png" alt="Logo" width="150" height="150" className='rounded-full'/>
-        <p className="text-[7rem] font-mono">
+        <p className="xl:text-[7rem] text-[4rem] font-mono">
         Persona
         </p>       
-        <p className="text-[7rem] font-mono">
+        <p className="xl:text-[7rem] text-[4rem] font-mono">
         Bot
         </p>
         </div>
