4e8628660ff7bee073f81d4f6d5f387f7a2faacb
tokcide <89933773+tokcide@users.noreply.github.com>
2023-08-25 11:47:28 +0530

    add: prettier configs

---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+};
