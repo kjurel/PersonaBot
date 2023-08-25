bb7293d863bd8c4f42adc7e2be253c37fae38aec
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-26 04:20:36 +0530

    fix: Homescreen UI developments

--- app/components/question.tsx
+++ app/components/question.tsx
@@ -10,7 +10,7 @@ const Question: FC<QuestionProps> = (props) => {
   const abstts = useStore($abstts);
   return (
     <>
-      <div className="relative flex h-10 w-full min-w-[200px]">
+      <div className="relative flex h-10 w-full min-w-[200px] my-2">
         <input
           ref={qestt} type="text"
           className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-800 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-white focus:border-t-white focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
@@ -18,7 +18,7 @@ const Question: FC<QuestionProps> = (props) => {
           required
         />
         <button
-          className="rounded-full  !absolute right-1 top-1 z-10 select-none rounded bg-blue-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
+          className=" rounded-lg !absolute right-1 top-1 z-10 select-none bg-blue-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
           onClick={() => {
             if (abstts === "") {
               // normal question
