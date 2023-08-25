f4b199661b8b7a75cfa84b6cc1b30ff0eadc3fb2
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 12:49:00 +0530

    add: Homescreen updated with image

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -1,15 +1,19 @@
+import Image from 'next/image'
 export default function Homepage() {
   const navbarHeight = 4;
   const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   return (
     <div className='relative' style={{ height: availableHeight }}>
       <div className='bg-gradient-to-r from-cyan-500 to-blue-500 absolute left-0 h-full w-[50%]'>
-        <p>
+        <div className="flex items-center justify-center flex-col">
+        <Image src="/logo.png" alt="Logo" width="250" height="250"/>
+        <p className="text-[7rem] font-mono flex-1">
         Persona
-        </p>
-        <p>
+        </p>       
+        <p className="text-[7rem] font-mono flex-1">
         Bot
         </p>
+        </div>
       </div>
       <div className='bg-gradient-to-r from-blue-500 to-indigo-500 absolute right-0 h-full w-[50%]'>
       </div>
