3001259ec2eb12198e9db12c5bbddfaff913fc71
tokcide <89933773+tokcide@users.noreply.github.com>
2023-08-25 13:19:54 +0530

    add: parentId to api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,16 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ChatGPTAPI } from "chatgpt";
 
-async function example() {
+async function wrapper(prompt: string, parentId?: string) {
   const api = new ChatGPTAPI({
     apiKey: process.env.OPEN_API_KEY!,
   });
 
-  const res = await api.sendMessage("Say hello to me in 5 wrods!");
-  return res.text;
+  const res = await api.sendMessage(prompt, { parentMessageId: parentId });
+  return res;
 }
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  example().then((answer) => {
-    res.status(200).json({ body: answer });
-  });
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  const {
+    query: { prompt, parentId },
+    method,
+  } = request;
+
+  if (!prompt) return response.status(404).send("`promt` is required.");
+  else if (Array.isArray(prompt) || Array.isArray(parentId))
+    return response
+      .status(400)
+      .send("`prompt` and `parentId` should be passed only once.");
+
+  wrapper(prompt, parentId).then((answer) =>
+    response
+      .status(200)
+      .json({ answer: answer.text, parentId: answer.parentMessageId }),
+  );
 };
