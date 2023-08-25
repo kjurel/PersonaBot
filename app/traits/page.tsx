545157aea404fb78616a758f436322ab8e4d2048
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 16:23:48 +0530

    fix: update on traits

--- app/traits/page.tsx
+++ app/traits/page.tsx
@@ -5,9 +5,12 @@ export default function Home() {
   const navbarHeight = 4;
   const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   return (
-    <div style={{ height: availableHeight }}>
+    <div >
       <Navbar />
-      <div className='flex -space-x-2 inset-x-0 bottom-0'>
+      <div style={{ height: availableHeight }}>
+        <div></div>
+        <div className='flex inset-x-0 bottom-0'>
+
         <input
           type="text"
           style={{
@@ -17,10 +20,11 @@ export default function Home() {
             boxSizing: 'border-box',
           }}
           className="rounded-l-lg"
-        />
+          />
         <button className="bg-green-500 hover:bg-green-600 text-white py-4 px-8 text-center text-lg font-semibold rounded-r-lg w-[15%]">
           Click me
         </button>
+          </div>
       </div>
     </div>
   );
