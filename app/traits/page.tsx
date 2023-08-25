d69293b1d8cdac8b81c1e474cc82fd0e90f3680c
Aarat Swaroop <aarat2018@gmail.com>
2023-08-25 19:03:14 +0530

    add: Changes to Trait Grid

--- app/traits/page.tsx
+++ app/traits/page.tsx
@@ -1,19 +1,117 @@
-import React from 'react';
-import Navbar from '../components/navbar';
-
+import React from "react";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   const navbarHeight = 4;
   const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   const user = null; // You can replace null with an actual user object
 
+  const personalitySections = [
+    {
+      id: "architect",
+      name: "Architect",
+      description: "INTJ-A / INTJ-T",
+      backgroundImage: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.16personalities.com%2Farticles%2Fproductivity-tips-for-architects-intjs-handling-the-routine&psig=AOvVaw3hMqiJhRoQtC1hVVwB47ym&ust=1693056684227000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKigtuP194ADFQAAAAAdAAAAABAE",
+    },
+    {
+      id: "logician",
+      name: "Logician",
+      description: "INTP-A / INTP-T",
+      backgroundImage: "URL_TO_LOGICIAN_IMAGE",
+    },
+    {
+      id: "commander",
+      name: "Commander",
+      description: "ENTJ-A / ENTJ-T",
+      backgroundImage: "URL_TO_COMMANDER_IMAGE",
+    },
+    {
+      id: "debater",
+      name: "Debater",
+      description: "ENTP-A / ENTP-T",
+      backgroundImage: "URL_TO_DEBATER_IMAGE",
+    },
+    {
+      id: "advocate",
+      name: "Advocate",
+      description: "INFJ-A / INFJ-T",
+      backgroundImage: "URL_TO_ADVOCATE_IMAGE",
+    },
+    {
+      id: "mediator",
+      name: "Mediator",
+      description: "INFP-A / INFP-T",
+      backgroundImage: "URL_TO_MEDIATOR_IMAGE",
+    },
+    {
+      id: "protagonist",
+      name: "Protagonist",
+      description: "ENFJ-A / ENFJ-T",
+      backgroundImage: "URL_TO_PROTAGONIST_IMAGE",
+    },
+    {
+      id: "campaigner",
+      name: "Campaigner",
+      description: "ENFP-A / ENFP-T",
+      backgroundImage: "URL_TO_CAMPAIGNER_IMAGE",
+    },
+    {
+      id: "logistician",
+      name: "Logistician",
+      description: "ISTJ-A / ISTJ-T",
+      backgroundImage: "URL_TO_LOGISTICIAN_IMAGE",
+    },
+    {
+      id: "defender",
+      name: "Defender",
+      description: "ISFJ-A / ISFJ-T",
+      backgroundImage: "URL_TO_DEFENDER_IMAGE",
+    },
+    {
+      id: "executive",
+      name: "Executive",
+      description: "ESTJ-A / ESTJ-T",
+      backgroundImage: "URL_TO_EXECUTIVE_IMAGE",
+    },
+    {
+      id: "consul",
+      name: "Consul",
+      description: "ESFJ-A / ESFJ-T",
+      backgroundImage: "URL_TO_CONSUL_IMAGE",
+    },
+    {
+      id: "virtuoso",
+      name: "Virtuoso",
+      description: "ISTP-A / ISTP-T",
+      backgroundImage: "URL_TO_VIRTUOSO_IMAGE",
+    },
+    {
+      id: "adventurer",
+      name: "Adventurer",
+      description: "ISFP-A / ISFP-T",
+      backgroundImage: "URL_TO_ADVENTURER_IMAGE",
+    },
+    {
+      id: "entrepreneur",
+      name: "Entrepreneur",
+      description: "ESTP-A / ESTP-T",
+      backgroundImage: "URL_TO_ENTREPRENEUR_IMAGE",
+    },
+    {
+      id: "entertainer",
+      name: "Entertainer",
+      description: "ESFP-A / ESFP-T",
+      backgroundImage: "URL_TO_ENTERTAINER_IMAGE",
+    },
+  ];
+  
+
   return (
     <div>
       <Navbar />
       <div style={{ height: availableHeight }}>
-        
         {user ? (
-          <div className='flex inset-x-0 bottom-0'>
+          <div className="flex inset-x-0 bottom-0">
             <input
               type="text"
               style={{
@@ -30,43 +128,37 @@ export default function Home() {
           </div>
         ) : (
           <div>
-            <div className="grid grid-cols-4 gap-4" style={{ height: availableHeight }}>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Architect</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">INTJ-A / INTJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Logician</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">INTP-A / INTP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Commander</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ENTJ-A / ENTJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Debater</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ENTP-A / ENTP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Advocate</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">INFJ-A / INFJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mediator</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">INFP-A / INFP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Protagonists</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ENFJ-A / ENFJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Campaigner</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ENFP-A / ENFP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Logistician</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ISTJ-A / ISTJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Defender</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ISFJ-A / ISFJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Executive</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ESTJ-A / ESTJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Consul</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ESFJ-A / ESFJ-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Virtuoso</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ISTP-A / ISTP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Adventurer</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ISFP-A / ISFP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Entrepreneur</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ESTP-A / ESTP-T </p></div>
-              <div><h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Entertainer</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">ESFP-A / ESFP-T </p></div>
+            <div
+              className="grid grid-cols-4 gap-4"
+              style={{ height: availableHeight }}
+            >
+              {personalitySections.map((section) => (
+                <div
+                  key={section.id}
+                  id={section.id}
+                  style={{
+                    backgroundImage: `url(${section.backgroundImage})`,
+                    backgroundSize: "100%",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "20px",
+                  }}
+                >
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+                    {section.name}
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    {section.description}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         )}
-
       </div>
     </div>
   );
