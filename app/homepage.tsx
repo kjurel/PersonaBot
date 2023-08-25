4f43dda100cfb267f4749f81eed840672e10a90b
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 12:20:24 +0530

    add: homescreen design

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -1,5 +1,18 @@
 export default function Homepage() {
+  const navbarHeight = 4;
+  const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   return (
-    <div>hello homepage</div>
+    <div className='relative' style={{ height: availableHeight }}>
+      <div className='bg-gradient-to-r from-cyan-500 to-blue-500 absolute left-0 h-full w-[50%]'>
+        <p>
+        Persona
+        </p>
+        <p>
+        Bot
+        </p>
+      </div>
+      <div className='bg-gradient-to-r from-blue-500 to-indigo-500 absolute right-0 h-full w-[50%]'>
+      </div>
+  </div>
   )
 }
