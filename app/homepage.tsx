32195f3fedb8576948f013383e42a06fa990a4ff
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-25 14:09:17 +0530

    fix: Homepage made reponsive

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -4,7 +4,7 @@ export default function Homepage() {
   const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
   return (
     <div className='relative flex flex-col md:flex-row' style={{ height: availableHeight }}>
-      <div className='bg-gradient-to-r from-cyan-500 to-blue-500 absolute left-0 md:h-full md:w-[50%] items-center flex justify-center h-[50%] w-full'>
+      <div className='md:bg-gradient-to-r from-cyan-500 to-blue-500 absolute left-0 md:h-full md:w-[50%] items-center flex justify-center h-[50%] w-full bg-gradient-to-b from-cyan-500 to-blue-500'>
         <div className="flex items-center justify-center flex-col">
         <Image src="/logo.png" alt="Logo" width="150" height="150" className='rounded-full'/>
         <p className="text-[7rem] font-mono">
@@ -15,7 +15,7 @@ export default function Homepage() {
         </p>
         </div>
       </div>
-      <div className='bg-gradient-to-r from-blue-500 to-indigo-500 absolute right-0 md:h-full md:w-[50%] h-[50%] w-full bottom-0 '>
+      <div className='md:bg-gradient-to-r from-blue-500 to-indigo-500 absolute right-0 md:h-full md:w-[50%] h-[50%] w-full bottom-0 bg-gradient-to-b from-blue-500 to-indigo-500'>
       </div>
   </div>
   )
